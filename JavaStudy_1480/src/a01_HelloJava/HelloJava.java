829a0accad947c3501a1b4320ee747664113a580
= <=>
2022-02-21 11:13:55 +0900

    첫수업 HelloJava 출력해보기

---
+++ JavaStudy_1480/src/a01_HelloJava/HelloJava.java
@@ -0,0 +1,38 @@
+package a01_HelloJava;
+
+/**
+ * 
+ *	여러줄 주석(클래스 또는 메소드를 설명하는 주석)
+ */
+/*
+ * 여러줄 주석(여러줄 메모용)
+ */
+// 한 줄 주석.
+
+// 주석의 단축키 Ctrl +/
+
+public class HelloJava {
+
+/**
+ * 
+ * 프로그램의 시작 지점 main함수.
+ */
+	public static void main(String[] args) {
+                                               //		'문자' -> 'A'
+//		"문자열" -> "HELLOJAVA"
+		System.out.println("hello, java");
+		System.out.print("박현중\n\n");
+		System.out.print("이름 : ");
+		System.out.println("박현중");
+		System.out.print("연락처 : ");
+		System.out.println("010-5133-8819");
+		System.out.print("주소 : ");
+		System.out.println("부산 해운대구 반여1동");
+/*
+ * 이름 : 박현중
+ * 연락처 : 01051338819
+ * 주소 : 부산 해운대구 반여1동		
+ */
+	}
+	
+}
