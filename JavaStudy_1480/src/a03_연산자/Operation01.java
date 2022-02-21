1a7ae34e35001c10c001a75f7b07f98fd9eeb20c
= <=>
2022-02-21 11:17:06 +0900

    연산자 수업내용

---
+++ JavaStudy_1480/src/a03_연산자/Operation01.java
@@ -0,0 +1,14 @@
+package a03_연산자;
+
+public class Operation01 {
+
+	public static void main(String[] args) {
+		int score ;
+	
+		for(score = 0; score<10; score++) {
+			System.out.println(score);
+		}
+	
+	}
+
+}
