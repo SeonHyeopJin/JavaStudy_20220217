ddac1f91c3b970e687df78c4d6a33faaa9e600be
= <=>
2022-02-21 12:28:50 +0900

    연산차 수업 추가 내용

--- JavaStudy_1480/src/a03_연산자/Operation01.java
+++ JavaStudy_1480/src/a03_연산자/Operation01.java
@@ -3,12 +3,14 @@ package a03_연산자;
 public class Operation01 {
 
 	public static void main(String[] args) {
-		int score ;
-	
-		for(score = 0; score<10; score++) {
-			System.out.println(score);
+        //증(가)감(소)연산자		 
+		int score = 10;
+	    //score = score +1;
+        System.out.println(score++);		
+		System.out.println(score--);
+		System.out.println(score);
+		System.out.println(score++);
+		System.out.println(score);
 		}
 	
 	}
-
-}
