f79c278a7057d5484f2b3a52d278b0d7e5066d01
C:\junil\seonhyeop <jsh912001@naver.com>
2022-02-25 12:57:48 +0900

    함수

---
+++ JavaStudy_1480/src/a08/함수/FunctionEx.java
@@ -0,0 +1,23 @@
+package a08.함수;
+
+public class FunctionEx {
+
+	public static int calc(int x, int y, int z) {
+		int result = x + y;
+        int result2 = result * z;
+        return result2;
+	}
+	public static void main(String[] args) {
+		
+		System.out.println(calc(10, 20, 30));
+		System.out.println(calc(10, 20, 30));
+		System.out.println(calc(10, 20, 30));
+		System.out.println(calc(10, 20, 30));
+		System.out.println(calc(10, 20, 30));
+		System.out.println(calc(10, 20, 30));
+
+		
+		
+	}
+
+}
